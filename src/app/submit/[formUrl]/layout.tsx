b8fcfd0a4100641d526cc63ed4f7b1ecf4535ea4
Theuns Barnardt <Theuns.Barnardt@sasfin.com>
2024-08-20 12:29:45 +0200

    First Commit

---
+++ src/app/submit/[formUrl]/layout.tsx
@@ -0,0 +1,18 @@
+import ThemeSwitcher from "@/components/designer/ThemeSwitcher";
+import React, { ReactNode } from "react";
+
+const Layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="flex flex-col min-h-screen min-w-full bg-background max-h-screen">
+      <nav className="flex justify-between items-center border-b border-border h-[60px] px-4 py-2">
+
+        <div className="flex gap-4 items-center">
+          <ThemeSwitcher />
+        </div>
+      </nav>
+      <main className="flex w-full flex-grow">{children}</main>
+    </div>
+  );
+};
+
+export default Layout;
